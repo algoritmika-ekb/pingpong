0843a83f39bab6e2906fe00794ce1a3043b48773
algoritmika-ekb <pashamikov@hotmail.com>
2026-04-19 16:27:53 +0500

    Add files via upload

---
+++ про.py
@@ -0,0 +1,46 @@
+# без разширения алгоритмики
+from pygame import *
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        sprite.Sprite.__init__(self)    
+
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
+back = (200, 255, 255)
+window = display.set_mode((600, 500))
+window.fill(back)
+
+game = True
+finish = False
+clock = clock.Clock()
+FPS = 60
+
+
+racket1 = Player('racket.png', 30, 200, 4, 50, 150)
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
+ball = GameSprite('tennis_ball.pnsg', 200, 200, 4, 50, 50)
+
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (100, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (100, 0, 0))
